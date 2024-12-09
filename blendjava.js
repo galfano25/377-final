d935b6a6df7e3778d074eee71e78dedeb1c3825d
Nhi Pham <nhipham@terpmail.umd.edu>
2024-12-09 18:15:06 -0500

    coded the genre button to change colors and text when clicked, created a function in the js for the button

--- blendjava.js
+++ blendjava.js
@@ -1,3 +1,22 @@
 simpleslider.getSlider({
   container: document.getElementById("BTSSlider"),
 });
+
+function selectGenre(chosenGenre) {
+  const button = document.getElementById('genreButton')
+  button.textContent = chosenGenre
+
+  const genreColors = {
+    Pop: '#f1c5ae',
+    Rock: '#ecddd0',
+    Rap: '#ced2c2',
+    'R&B': '#92b1b6',
+    EDM: '#35455d',
+    Country: '#bfd1df',
+    Jazz: '#e8d0a9',
+    Classical: '#c1dad6',
+    Indie: '#6d929',
+  };
+
+  button.style.backgroundColor = genreColors[chosenGenre] || '#898989';
+}
